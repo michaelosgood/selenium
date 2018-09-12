42661b7ea02335ea74b02515d1d87d4913b18055
Michael Osgood <michael.f.osgood@gmail.com>
2018-09-11 21:43:09 -0700

    added code to input username and password

--- google_auto.js
+++ google_auto.js
@@ -1,5 +1,20 @@
+
+
 var webdriver = require('selenium-webdriver');
 
 var driver = new webdriver.Builder().forBrowser('chrome').build();
 
 driver.get('https://web.prescribewellness.com/');
+
+inputField = driver.findElement(webdriver.By.id('mbr-uid'));
+
+var useremail = "mosgood@prescribewellness.com";
+
+inputField.sendKeys(useremail);
+
+var password = "TestPW";
+
+inputField = driver.findElement(webdriver.By.id('mbr-pwd'));
+
+inputField.sendKeys(password);
+
