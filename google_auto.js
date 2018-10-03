6449822c3b51aedc5d7a149c04c151f604400137
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-02 17:48:39 -0700

    updating test script

--- google_auto.js
+++ google_auto.js
@@ -1,7 +1,9 @@
 var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
+var assert = require ('assert');
 
 driver.get('https://web.prescribewellness.com/');
 
