bc394b69ad2d809ac9b5b67dcb38cda4a0e0dfab
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-02 18:05:37 -0700

    added new test script

---
+++ google_authentication.js
@@ -0,0 +1,20 @@
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
+var assert = require ('assert');
+
+test.describe('User Authentication', function () {
+
+    test.it('User can sign in', function () {
+        driver.get('https://web.prescribewellness.com/');
+        inputField = driver.findElement(webdriver.By.id('mbr-uid'));
+        var useremail = "h3n2@prescribewellness.com";
+        inputField.sendKeys(useremail);
+        var password = "Influenza123!";
+        inputField = driver.findElement(webdriver.By.id('mbr-pwd'));
+         inputField.sendKeys(password);
+        driver.findElement(webdriver.By.id('login')).click();
+    });
+});
