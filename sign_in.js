5ed4298a637fe51247296adc4f8b9c1428b04c63
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-14 18:44:19 -0700

    updated var to const

--- sign_in.js
+++ sign_in.js
@@ -1,19 +1,19 @@
-var webdriver = require('selenium-webdriver');
-var test = require('selenium-webdriver/testing');
-var driver = new webdriver.Builder()
+const webdriver = require('selenium-webdriver');
+const test = require('selenium-webdriver/testing');
+const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
-var assert = require ('assert');
+const assert = require ('assert');
 
 driver.get('https://web.prescribewellness.com/');
 
 inputField = driver.findElement(webdriver.By.id('mbr-uid'));
 
-var useremail = "h3n2@prescribewellness.com";
+const useremail = "h3n2@prescribewellness.com";
 
 inputField.sendKeys(useremail);
 
-var password = "Influenza123!#";
+const password = "Influenza123!#";
 
 inputField = driver.findElement(webdriver.By.id('mbr-pwd'));
 
