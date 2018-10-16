9458011548cdf5b8ac92e8715b5f32019a902778
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-15 20:30:08 -0700

    added firefox test

---
+++ firefox_authentication.js
@@ -0,0 +1,10 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.firefox()).build();
+
+driver.get('https://web.prescribewellness.com/');
+driver.findElement(webdriver.By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
+driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys('Influenza123!#');
+driver.findElement(webdriver.By.id('login')).click();
+
