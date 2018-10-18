2c6bae5f8052cc342ef5839aff05aa114119d5c6
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-17 19:58:49 -0700

    updated ondemand

--- ondemand.js
+++ ondemand.js
@@ -8,6 +8,7 @@ const assert = require ('assert');
 driver.get('https://web.prescribewellness.com/');
 driver.findElement(webdriver.By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
 driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys('Influenza123!#');
-var started = driver.findElement(webdriver.By.id('login')).click();
-driver.wait(started, 5 * 1000, 'Server should load within 5 seconds');
+driver.findElement(webdriver.By.id('login')).click();
+
 // driver.findElement(webdriver.By.id('pwTopGearIcon'))
+
