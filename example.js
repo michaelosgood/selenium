8f0ef3d728bff82c0d69d140af596762c0bddc4c
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-22 22:42:45 -0700

    added ex2

--- example.js
+++ example.js
@@ -1,10 +1,13 @@
-var webdriver = require ('selenium-webdriver'),
-    By = webdriver.By,
-    until = webdriver.until;
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-    var driver = new webdriver.Builder().forBrowser('chrome').build();
-    driver.get('https://web.prescribewellness.com');
-    driver.findElement(By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
-    driver.findElement(By.id('mbr-pwd')).sendKeys('Influenza123!#');
-    driver.findElement(By.id('login')).click();
-    
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://web.prescribewellness.com');
+    await driver.findElement(By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
+    await driver.findElement(By.id('mbr-pwd')).sendKeys('Influenza123!#', Key.RETURN);
+    await driver.wait(until.titleIs('webdriver - Google Search'), 3000);
+  } finally {
+    await driver.quit();
+  }
+})();
