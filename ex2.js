961730252a6855d3bd9cec158f2d7637809d2436
Michael Osgood <michael.f.osgood@gmail.com>
2018-10-23 14:13:51 -0700

    updated ondemand.js

--- ex2.js
+++ ex2.js
@@ -6,8 +6,9 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
     await driver.get('https://web.prescribewellness.com');
     await driver.findElement(By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
     await driver.findElement(By.id('mbr-pwd')).sendKeys('Influenza123!#', Key.RETURN);
-    await driver.wait(until.titleIs('webdriver - Google Search'), 3000);
+    await driver.findElement(By.id('pwTopGearIcon')).click();
+    await driver.findElement(By.linkText('OnDemand')).click();
   } finally {
-    await driver.quit();
+    // await driver.quit();
   }
 })();
--- ondemand.js
+++ ondemand.js
@@ -1,14 +1,15 @@
-const webdriver = require('selenium-webdriver');
-const test = require('selenium-webdriver/testing');
-let driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
-const assert = require ('assert');
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-driver.get('https://web.prescribewellness.com/');
-driver.findElement(webdriver.By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
-driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys('Influenza123!#');
-driver.findElement(webdriver.By.id('login')).click();
-
-// driver.findElement(webdriver.By.id('pwTopGearIcon'))
-
+(async function example() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    await driver.get('https://web.prescribewellness.com');
+    await driver.findElement(By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
+    await driver.findElement(By.id('mbr-pwd')).sendKeys('Influenza123!#', Key.RETURN);
+    await driver.findElement(By.id('pwTopGearIcon')).click();
+    await driver.findElement(By.linkText('OnDemand')).click();
+  } finally {
+    // Uncomment out when test script is complete
+    // await driver.quit();
+  }
+})();
