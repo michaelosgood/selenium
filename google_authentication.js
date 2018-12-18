2d9a1aa5d91e3c0c9568bc17058fbe1dfb1870c9
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-17 19:46:25 -0800

    updated test

--- google_authentication.js
+++ google_authentication.js
@@ -1,10 +1,21 @@
-var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder()
-    .forBrowser('chrome')
-    .build();
-var assert = require ('assert');
+// Updated Test
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { expect } = require('chai');
 
-driver.get('https://web.prescribewellness.com/');
-driver.findElement(webdriver.By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
-driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys('Influenza123!#');
-driver.findElement(webdriver.By.id('login')).click();
+describe('Google Authentication Test', () => {
+    const driver = new Builder().forBrowser('chrome').build();
+
+    it('should go to https://web.prescribewellness.com and check the title', async () => {
+        await driver.get('https://web.prescribewellness.com');
+        await driver.sleep(20000);
+        await driver.findElement(By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
+        await driver.findElement(By.id('mbr-pwd')).sendKeys('Influenza123!#');
+        await driver.findElement(By.id('login')).click();
+       
+        const title = await driver.getTitle();
+
+        expect(title).to.equal('Pharmacy Dashboard - Pharmacy Portal  - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2');
+    });
+
+    after(async () => driver.quit());
+});
