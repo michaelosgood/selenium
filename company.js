6ed54965e07e14a513b10a2b31dbf94028583ea6
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-19 11:32:31 -0800

    updated company.js

--- company.js
+++ company.js
@@ -3,7 +3,7 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 (async function company() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-        await driver.get('https://www.prescribewellness.com/business/company');
+        await driver.get('https://wwwstg.prescribewellness.com/business/company');
         await driver.wait(until.titleIs('Company | PrescribeWellness'), 1000);
         await console.log('Title has been verified');
 
