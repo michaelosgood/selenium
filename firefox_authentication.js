b00fbec7f07650152f5edb509d4c764b420536c0
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-19 15:18:39 -0800

    updated firefox_authentication.js

--- firefox_authentication.js
+++ firefox_authentication.js
@@ -1,8 +1,9 @@
-var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder().
+let webdriver = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.firefox()).build();
 
 driver.get('https://web.prescribewellness.com/');
-driver.findElement(webdriver.By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
-driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys('Influenza123!#');
+driver.findElement(webdriver.By.id('mbr-uid')).sendKeys(credentials.customer_user);
+driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys(credentials.customer_password);
 driver.findElement(webdriver.By.id('login')).click();
