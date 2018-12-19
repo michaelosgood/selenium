10b2ff8f4d69605676161708d9eece7c81a22856
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-19 15:29:36 -0800

    added environment.js

--- firefox_authentication.js
+++ firefox_authentication.js
@@ -1,9 +1,10 @@
 let webdriver = require('selenium-webdriver');
 let credentials = require('./credentials.js');
+let environment = require('./environment.js');
 let driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.firefox()).build();
 
-driver.get('https://web.prescribewellness.com/');
+driver.get(environment.prod);
 driver.findElement(webdriver.By.id('mbr-uid')).sendKeys(credentials.customer_user);
 driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys(credentials.customer_password);
 driver.findElement(webdriver.By.id('login')).click();
--- firefox_stg_signin.js
+++ firefox_stg_signin.js
@@ -1,9 +1,10 @@
 var webdriver = require('selenium-webdriver');
 let credentials = require('./credentials.js');
+let environment = require('./environment.js');
 var driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.firefox()).build();
 
-driver.get('https://webstg.prescribewellness.com/');
+driver.get(environment.stg);
 driver.findElement(webdriver.By.id('mbr-uid')).sendKeys(credentials.internal_user);
 driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys(credentials.internal_password);
 driver.findElement(webdriver.By.id('login')).click();
