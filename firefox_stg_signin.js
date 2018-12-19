e4c7ea6e05df091d79b1e23680a7afdb54d6a14b
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-19 14:59:10 -0800

    hid user credentials in credentials.js

--- firefox_stg_signin.js
+++ firefox_stg_signin.js
@@ -1,8 +1,9 @@
 var webdriver = require('selenium-webdriver');
+let credentials = require('./credentials.js');
 var driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.firefox()).build();
 
 driver.get('https://webstg.prescribewellness.com/');
-driver.findElement(webdriver.By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
-driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys('Influenza123!#');
+driver.findElement(webdriver.By.id('mbr-uid')).sendKeys(credentials.internal_user);
+driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys(credentials.internal_password);
 driver.findElement(webdriver.By.id('login')).click();
