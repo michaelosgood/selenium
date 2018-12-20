b4bbea51264978039854f830743cb9f11b19c0fc
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-20 11:19:17 -0800

    updated sign_in.js

--- sign_in.js
+++ sign_in.js
@@ -1,22 +1,20 @@
 const webdriver = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let environment = require('./environment.js');
 
 const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
-driver.get('https://web.prescribewellness.com/');
+driver.get(environment.stg);
 
 inputField = driver.findElement(webdriver.By.id('mbr-uid'));
 
-const useremail = "h3n2@prescribewellness.com";
-
-inputField.sendKeys(useremail);
-
-const password = "Influenza123!#";
+inputField.sendKeys(credentials.internal_user);
 
 inputField = driver.findElement(webdriver.By.id('mbr-pwd'));
 
-inputField.sendKeys(password);
+inputField.sendKeys(credentials.internal_password);
 
 driver.findElement(webdriver.By.id("login")).click();
 
