bbb65cc6b39d1c56e930fd75561a831dd735314d
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-20 11:23:25 -0800

    updatd ex2 and example

--- example.js
+++ example.js
@@ -1,11 +1,13 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let environment = require('./environment.js');
 
 (async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
-    await driver.get('https://web.prescribewellness.com');
-    await driver.findElement(By.id('mbr-uid')).sendKeys('h3n2@prescribewellness.com');
-    await driver.findElement(By.id('mbr-pwd')).sendKeys('Influenza123!#', Key.RETURN);
+    await driver.get(environment.stg);
+    await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+    await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
     await driver.findElement(By.id('pwTopGearIcon')).click();
   } finally {
     // await driver.quit();
