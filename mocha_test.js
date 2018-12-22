9142d5f13047b2989a53b5bfd6c72d849217aeb7
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-22 12:29:55 -0800

    added google sigin for stg

--- mocha_test.js
+++ mocha_test.js
@@ -1,4 +1,4 @@
-describe('Array', function() {
+describe('Mocha Array Test', function() {
     describe('#indexOf()', function() {
       it('should return -1 when the value is not present', function() {
         [1,2,3].indexOf(5).should.equal(-1);
