9944dc95ecd964dacb2125a0dccb09bde8b9542d
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-26 15:48:34 -0800

    fixed tests for company.js and ex2.js

--- company.js
+++ company.js
@@ -1,13 +1,20 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
+let environment = require('./environment.js');
+let assert = require('assert');
+let driver = new Builder().forBrowser('chrome').build();
 
-(async function company() {
-  let driver = await new Builder().forBrowser('chrome').build();
-  try {
-        await driver.get('https://wwwstg.prescribewellness.com/business/company');
-        await driver.wait(until.titleIs('Company | PrescribeWellness'), 1000);
-        await console.log('Title has been verified');
 
-  } finally {
-        await driver.quit();
-  }
-})();
+describe('Go to Public Site and Verify Title for the Company page', function() {
+
+  it('should allow users to navigate to: "www.prescribewellness.com/business/company Page"', function () {
+    driver.get('https://www.prescribewellness.com/business/company');
+  })
+  it('should verify the title displays as: "Company | PrescribeWellness"', function() {
+    driver.getTitle().then(function(title) {
+      assert.equal(title, 'Company | PrescribeWellness');
+    })
+  })
+  it('should quit webdriver after verifying title', function() {
+    driver.quit();
+  })
+})
--- ex2.js
+++ ex2.js
@@ -6,12 +6,12 @@ let driver = new Builder().forBrowser('chrome').build();
 
 describe('Go to Public Site and Verify Title', function() {
 
-  it('should allow users to navigate to WWW Page', function () {
+  it('should navigate to WWW Page', function () {
     driver.get(environment.public);
   })
-  it('should verify the title displays as "Home | PrescribeWellness"', function() {
+  it('should verify the title displays: "Home | PrescribeWellness"', function() {
     driver.getTitle().then(function(title) {
-      assert.equal(title, "Home | PrescribeWellness");
+      assert.equal(title, "Home | PrescribeWellness")
     })
   })
   it('should quit webdriver after verifying title', function() {
