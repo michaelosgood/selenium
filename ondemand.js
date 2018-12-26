2363e40ac4900efd89accf51efdff6f08bd71d75
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-26 11:06:08 -0800

    updated ondemand js

--- ondemand.js
+++ ondemand.js
@@ -14,9 +14,13 @@ let environment = require('./environment.js');
     await driver.findElement(By.id('phone_numbers')).sendKeys('3036534679');
     await console.log('Phone Number has been entered');
     await driver.findElement(By.css('.btn-pw')).click();
-    await driver.findElement(By.xpath('//*[@id="onDemandModalWindowArea"]/div/div[2]/div/div/div[3]/div[3]/div[2]/div[1]/div[3]/button')).click();
+    await driver.findElement(By.tagName('button')).findElement(By.linkText('Select')).click();
+
   } finally {
     // Uncomment out when test script is complete
     // await driver.quit();
+    //*[@id="onDemandModalWindowArea"]/div/div[2]/div/div/div[3]/div[3]/div[2]/div[1]/div[3]/button
+    // #onDemandModalWindowArea > div > div.modal-dialog.ondemand-modal-dialog-wrapper > div > div > div.modal-demand-content > div.campaigns-table > div.campaigns-wrapper > div:nth-child(2) > div:nth-child(3) > button
+    // <button type="button" class="btn btn-pw btn-select-campaign">Select</button>
   }
 })();
