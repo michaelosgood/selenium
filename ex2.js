62fb74a553c5c0c87748ef64636fa023906d5532
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-26 15:39:37 -0800

    updaetd ex2.js

--- ex2.js
+++ ex2.js
@@ -1,15 +1,20 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
-let credentials = require('./credentials.js');
 let environment = require('./environment.js');
 let assert = require('assert');
 let driver = new Builder().forBrowser('chrome').build();
 
-describe('Ex2 Test', function() {
 
-  it('User can go to WWW Page', function () {
-    driver.get('https://www.prescribewellness.com/');
+describe('Go to Public Site and Verify Title', function() {
+
+  it('should allow users to navigate to WWW Page', function () {
+    driver.get(environment.public);
+  })
+  it('should verify the title displays as "Home | PrescribeWellness"', function() {
     driver.getTitle().then(function(title) {
       assert.equal(title, "Home | PrescribeWellness");
     })
+  })
+  it('should quit webdriver after verifying title', function() {
+    driver.quit();
   })
 })
