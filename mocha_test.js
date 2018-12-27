6436423b2fcf3bc1a6d0dc33f45c46afd1355610
Michael Osgood <michael.f.osgood@gmail.com>
2018-12-26 20:30:32 -0800

    updated mocha_test.js

--- mocha_test.js
+++ mocha_test.js
@@ -1,8 +1,17 @@
-describe('Mocha Array Test', function() {
-    describe('#indexOf()', function() {
-      it('should return -1 when the value is not present', function() {
-        [1,2,3].indexOf(5).should.equal(-1);
-        [1,2,3].indexOf(0).should.equal(-1);
-      });
+const { Builder, By, Key, until } = require('selenium-webdriver');
+const { expect } = require('chai');
+
+describe('MochaTest', () => {
+    const driver = new Builder().forBrowser('chrome').build();
+
+    it('should go to www.google.com/ncr, search for "cheese", and verify the title', async () => {
+        await driver.get('https://www.google.com/ncr');
+        await driver.findElement(By.name('q')).sendKeys('cheese', Key.RETURN);
+        const title = await driver.getTitle();
+
+        expect(title).to.equal('cheese - Google Search');
+        await console.log("The title is: 'cheese - Google Search'");
     });
-  });
+
+    after(async () => driver.quit());
+});
