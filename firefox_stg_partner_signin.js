df4e681fdf062a702afa48f76a15fe0274c1047c
Michael Osgood <michael.f.osgood@gmail.com>
2019-01-07 16:56:41 -0800

    updated firefox stg sign in

--- firefox_stg_partner_signin.js
+++ firefox_stg_partner_signin.js
@@ -6,6 +6,7 @@ let environment = require('./environment.js');
   let driver = await new Builder().forBrowser('firefox').build();
     try {
         await driver.get(environment.stg);
+        await driver.manage().window().maximize();
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.partner_user);
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.partner_password, Key.RETURN);
         await driver.findElement(By.id('pwTopGearIcon')).click();
