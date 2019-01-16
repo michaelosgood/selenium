b7d613a8151108f468119d9b5e7591f8cd185179
Michael Osgood <michael.f.osgood@gmail.com>
2019-01-15 16:25:25 -0800

    updated the other firefox scripts

--- firefox_authentication.js
+++ firefox_authentication.js
@@ -5,6 +5,7 @@ let driver = new webdriver.Builder().
     withCapabilities(webdriver.Capabilities.firefox()).build();
 
 driver.get(environment.stg);
+driver.manage().window().maximize();
 driver.findElement(webdriver.By.id('mbr-uid')).sendKeys(credentials.customer_user);
 driver.findElement(webdriver.By.id('mbr-pwd')).sendKeys(credentials.customer_password);
 driver.findElement(webdriver.By.id('login')).click();
