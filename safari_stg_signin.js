e76a995ee603d07a0f7d04a61c05e4c64b78b4fd
Michael Osgood <michael.f.osgood@gmail.com>
2019-01-29 11:54:12 -0800

    trying to fix safari scripts

--- safari_stg_signin.js
+++ safari_stg_signin.js
@@ -2,16 +2,12 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 let credentials = require('./credentials.js');
 let environment = require('./environment.js');
 
-(async function SafariStgSignIn() {
-  let driver = await new Builder().forBrowser('safari').build();
-    try {
-        await driver.get(environment.stg);
-        await driver.manage().window().maximize();
-        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
-        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
-        //await driver.findElement(By.id('pwTopGearIcon')).click();
-    } 
-    catch(err) {
-        console.log(err);
-   }
-})();
+(async function SafariExample() {
+  let driver = await new Builder()
+  .forBrowser('safari').build();
+
+  await driver.get(environment.stg)
+  await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user)
+  await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN)
+})
+();
