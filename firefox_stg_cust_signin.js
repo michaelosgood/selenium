b5be34b5eb7c1a5d8697afdc57713f3d630a43b3
Michael Osgood <michael.f.osgood@gmail.com>
2019-01-30 10:13:19 -0800

    added customer firefox sign in

--- firefox_stg_cust_signin.js
+++ firefox_stg_cust_signin.js
@@ -2,13 +2,13 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 let credentials = require('./credentials.js');
 let environment = require('./environment.js');
 
-(async function SignIn() {
+(async function CustomerSignIn() {
   let driver = await new Builder().forBrowser('firefox').build();
     try {
         await driver.get(environment.stg);
         await driver.manage().window().maximize();
-        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.partner_user);
-        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.partner_password, Key.RETURN);
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
     } 
     catch(err) {
         console.log(err);
