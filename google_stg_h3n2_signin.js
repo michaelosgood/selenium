4980866d185b3ded902ca3ae4473630737ddd167
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-01 10:03:49 -0800

    updated voiceport scripts

---
+++ google_stg_h3n2_signin.js
@@ -0,0 +1,20 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let environment = require('./environment.js');
+
+(async function SignIn() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get(environment.stg);
+        await driver.manage().window().maximize();
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("Customer user signed into Production with Chrome");
+    }
+})();
