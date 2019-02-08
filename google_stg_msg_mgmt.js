0927d6d562f3e3faa6188ce21dd6f096cebb5479
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-07 17:06:13 -0800

    updated msg mgmt script

--- google_stg_msg_mgmt.js
+++ google_stg_msg_mgmt.js
@@ -6,12 +6,19 @@ let environment = require('./environment.js');
 (async function msgMgmtTotal() {
   let driver = await new Builder().forBrowser('chrome').build();
     try {
+        console.log("Initiating Message Management Test");
         await driver.get(environment.stg);
+        console.log("1. Went to Staging");
         await driver.manage().window().maximize();
+        console.log("2. Maximized the window");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internalTwo_user);
+        console.log("3. Entered internal two username");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internalTwo_password, Key.RETURN);
+        console.log("4. Entered password and clicked 'Enter'");
         await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("5. Clicked on the gear icon");
         await driver.findElement(By.linkText('Message Management')).click();
+        console.log("6. Selected 'Message Management'");
         await driver.findElement(By.className('k-pager-info k-label'));
         await driver.sleep(6000); // Wait for page to load
         await driver.takeScreenshot().then(function(data) { // Take Screenshot
@@ -22,6 +29,6 @@ let environment = require('./environment.js');
         console.log(err);
     }
     finally {
-        console.log("Internal 2 user went to Message Managment in Staging using Chrome");
+        console.log("Results: Internal 2 user went to Message Managment in Staging using Chrome");
     }
 })();
