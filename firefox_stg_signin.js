c733998142b0dee5501a2b147831ea6a4b4089a0
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-13 14:24:20 -0800

    updated firefox stg tst

--- firefox_stg_signin.js
+++ firefox_stg_signin.js
@@ -5,13 +5,20 @@ let environment = require('./environment.js');
 (async function SignIn() {
   let driver = await new Builder().forBrowser('firefox').build();
     try {
+        console.log("Initiating Firefox Stg Test");
         await driver.get(environment.stg);
+        console.log("1. Went to Staging");
         await driver.manage().window().maximize();
+        console.log("2.Maximized the window");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+        console.log("3 Entered username");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
-        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("4. Entered password and clicked 'return'");
     } 
     catch(err) {
         console.log(err);
+    }
+    finally {
+        console.log("Results: Internal successfully signed into Staging using Firefox");
     }
 })();
