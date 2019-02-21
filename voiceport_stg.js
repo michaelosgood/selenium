3b011c7c399a6c215ade3772e1b2f136eee089af
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-21 09:57:48 -0800

    updated login as script

--- voiceport_stg.js
+++ voiceport_stg.js
@@ -5,10 +5,15 @@ let environment = require('./environment.js');
 (async function VoiceportSignIn() {
   let driver = await new Builder().forBrowser('chrome').build();
     try {
+        console.log("Initiating VoicePort Sign In Test in Chrome");
         await driver.get(environment.stg);
+        console.log("1. Went to Staging: √");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.voiceport_user);
+        console.log("2. Entered VoicePort username: √");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.voiceport_password, Key.RETURN);
+        console.log("3. Entered password and clicked 'Enter': √");
         await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("4. Clicked on the gear icon: √");
     } 
     catch(err) {
         console.log(err);
