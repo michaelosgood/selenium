32459d1549483f15cc6561a99112390571a155e7
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-21 10:40:23 -0800

    fixed ondemand sms script

--- ondemand.js
+++ ondemand.js
@@ -5,22 +5,38 @@ let environment = require('./environment.js');
 (async function ondemand() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
+    console.log("Initiating OnDemand Test in Chrome");
     await driver.get(environment.prod);
+    console.log("1. Went to Production √");
     await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+    console.log("2. Entered customer username: √");
     await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
-    await console.log('Login Successful');
+    console.log("3. Entered password and clicked 'Enter': √");
+    await driver.sleep(6000); // Wait for page to load
     await driver.findElement(By.id('pwTopGearIcon')).click();
+    console.log("4. Clicked on the gear icon: √");
     await driver.findElement(By.linkText('OnDemand')).click();
+    console.log("5. Selected 'OnDemand': √");
+    await driver.sleep(6000); // Wait for page to load
     await driver.findElement(By.id('phone_numbers')).sendKeys('3036534679');
-    await console.log('Phone Number has been entered');
+    console.log("6. Entered a phone number √");
+    await driver.sleep(6000); // Wait for page to load
     await driver.findElement(By.css('.btn-pw')).click();
-    await driver.findElement(By.tagName('button')).findElement(By.linkText('Select')).click();
+    console.log("7. Clicked on 'Next' button");
+    await driver.sleep(6000); // Wait for page to load
+    await driver.findElement(By.css('.btn-select-campaign')).click();
+    console.log("8. Clicked on 'Select' button");
+    await driver.sleep(6000); // Wait for page to load
+    await driver.findElement(By.css('.fright')).click();
+    console.log("9. Clicked on 'Send Message' button");
 
-  } finally {
-    // Uncomment out when test script is complete
-    // await driver.quit();
-    //*[@id="onDemandModalWindowArea"]/div/div[2]/div/div/div[3]/div[3]/div[2]/div[1]/div[3]/button
-    // #onDemandModalWindowArea > div > div.modal-dialog.ondemand-modal-dialog-wrapper > div > div > div.modal-demand-content > div.campaigns-table > div.campaigns-wrapper > div:nth-child(2) > div:nth-child(3) > button
-    // <button type="button" class="btn btn-pw btn-select-campaign">Select</button>
+  } 
+  catch(err) {
+    console.log(err);
+  }
+  finally {
+    await driver.sleep(6000); // Wait for page to load
+    console.log("TEST RESULTS: OnDemand SMS has been sent!! √ Passed QA!");
+    //await driver.quit()
   }
 })();
