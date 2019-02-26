ba5a2a22c3ce6ad9738004630a9ab76c76cb711b
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-25 18:52:54 -0800

    updated scriptsf

--- google_stg_sample.js
+++ google_stg_sample.js
@@ -7,8 +7,8 @@ let environment = require('./environment.js');
     try {
         await driver.get(environment.stg);
         console.log("1. Went to Staging √");
-       await driver.manage().window().maximize();
-       console.log("2. Maximized the window √");
+        await driver.manage().window().maximize();
+        console.log("2. Maximized the window √");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.sample_user);
         console.log("3. Entered sample username √");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.sample_password, Key.RETURN);
