1a551920991678f26ad313ff57ab279ba8c0ae46
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-27 10:59:59 -0800

    updated stg firefox script

--- firefox_stg_signin.js
+++ firefox_stg_signin.js
@@ -14,6 +14,9 @@ let environment = require('./environment.js');
         console.log("3 Entered username √");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
         console.log("4. Entered password and clicked 'return' √");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("5. Clicked on the gear icon: √");
     } 
     catch(err) {
         console.log(err);
