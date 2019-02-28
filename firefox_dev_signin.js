49d9a572a13789fecfb56c56778fd8e151a06c19
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-28 10:24:32 -0800

    added admin script and updated dev firefox signin

--- firefox_dev_signin.js
+++ firefox_dev_signin.js
@@ -2,19 +2,26 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 let credentials = require('./credentials.js');
 let environment = require('./environment.js');
 
-(async function SignIn() {
+(async function FirefoxSignIn() {
   let driver = await new Builder().forBrowser('firefox').build();
     try {
+        console.log("Initiating Firefox DEV Test");
         await driver.get(environment.dev);
+        console.log("1. Went to Staging √");
         await driver.manage().window().maximize();
+        console.log("2. Maximized the window √");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+        console.log("3 Entered username √");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
+        console.log("4. Entered password and clicked 'return' √");
+        await driver.sleep(6000); // Wait for page to load
         await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("5. Clicked on the gear icon: √");
     } 
     catch(err) {
         console.log(err);
     }
     finally {
-        console.log("Internal user Signed into Dev with Firefox");
+        console.log("TEST RESULTS: Internal successfully signed into DEV using Firefox");
     }
 })();
---
+++ google_stg_admin.js
@@ -0,0 +1,46 @@
+const {Builder, By, Key, actions, until} = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let environment = require('./environment.js');
+let assert = require("chai").assert;
+
+(async function AdminTest() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        console.log("Initiating Message Management Test in Chrome");
+        await driver.get(environment.stg);
+        console.log("1. Went to Staging: √");
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+        console.log("2. Entered internal username: √");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
+        console.log("3. Entered password and clicked 'Enter': √");
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("4. Clicked on the gear icon: √");
+        await driver.findElement(By.linkText('Message Management')).click();
+        console.log("5. Selected 'Message Management': √");
+        await driver.findElement(By.className('k-pager-info k-label'));
+        await driver.sleep(6000); // Wait for page to load
+        console.log("6. Waited a couple seconds");
+        await driver.getTitle().then(function(title) {
+            assert.equal(title, "Message Management - Pharmacy Portal - mosgood");
+            console.log("Title for Message Managment is: " + title );
+        });
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("7. Clicked on the gear icon: √");
+        await driver.findElement(By.linkText('Campaign Management')).click();
+        console.log("8. Selected 'Campaign Managment': √");
+        await driver.findElement(By.className('k-pager-info k-label'));
+        await driver.sleep(6000); // Wait for page to load
+        console.log("9. Waited a couple seconds");
+        await driver.getTitle().then(function(title) {
+            assert.equal(title, "Campaign Management - Pharmacy Portal - mosgood");
+            console.log("Title for Campaign Managment is: " + title );
+        });
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("TEST RESULTS: Title page for 'Message Management' has been verified - √ Passed QA!");
+        await driver.quit()
+    }
+})();
