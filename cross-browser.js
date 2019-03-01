01cbeec639c5490d3de353e193d7bb2ada43d970
Michael Osgood <michael.f.osgood@gmail.com>
2019-02-28 18:39:40 -0800

    working on cross-browser test script

--- cross-browser.js
+++ cross-browser.js
@@ -1,5 +1,5 @@
 //-----CROSS BROWSER SIGN IN TEST -------------//
-// Type 'mocha sign_in.js' in the command line and press 'Enter' on keyboard execute test
+// Type 'mocha cross-broswer.js' in the command line and press 'Enter' on keyboard execute test
 const assert = require('assert');
 
 const {Browser, By, Key, until} = require('selenium-webdriver');
@@ -11,15 +11,14 @@ suite(function(env) {
   describe('Sign in to Staging', async function() {
     let driver;
 
-//    before(async function() {
+    before(async function() {
       // env.builder() returns a Builder instance preconfigured for the
       // envrionment's target browser (you may still define browser specific
       // options if necessary (i.e. firefox.Options or chrome.Options)).
-//      driver = await env.builder().build();
-//    });
+      driver = await env.builder().build();
+    });
 
     it('DEMO 1: CROSS BROWSER TEST', async function() {
-      driver = await env.builder().build();
       console.log("INITIATING CROSS BROWSER TEST");
       await driver.get(environment.stg);
       console.log("1. Went to Staging √");
@@ -30,12 +29,6 @@ suite(function(env) {
       await driver.sleep(6000); // Wait for page to load
     });
 
-    // it('demo 2: Go to google.com and verify title', async function() {
-    //   await driver.get('https://www.google.com/');
-    //   let url = await driver.getCurrentUrl();
-    //   assert.equal(url, 'https://www.google.com/');
-    // });
 
-    //after(() => driver && driver.quit());
   });
 });
