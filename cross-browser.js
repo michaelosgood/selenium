c01e997c19afb95226b0fa9d0f71a40e44bd3737
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-03 10:37:28 -0800

    fixed the cross-browser test

--- cross-browser.js
+++ cross-browser.js
@@ -9,28 +9,41 @@ let environment = require('./environment.js');
 
 suite(function(env) {
     describe('Sign in to Staging', async function() {
-
-        //it('DEMO 1: CROSS BROWSER TEST', async function() {
-            let driver = await env.builder().build();
+        beforeEach(function(done) {
+            this.timeout(30000); // A very long environment setup.
+            setTimeout(done, 2500);
+          });
+          let driver = await env.builder().build();
+          try {
+            //it('DEMO 1: CROSS BROWSER TEST', async function() {
             console.log("INITIATING CROSS BROWSER TEST");
-            await driver.get(environment.stg);
-            console.log("1. Went to Staging √");
-            await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+            await driver.get(environment.prod);
+            console.log("1. Went to PROD √");
+            await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
             console.log("2. Entered username √");
-            await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
+            await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
             console.log("3. Entered password and clicked 'Enter' √");
-            await driver.sleep(1500); // Wait for page to load
+            await driver.sleep(5000); // Wait for page to load
             await driver.findElement(By.id('pwTopGearIcon')).click();
             console.log("4. Clicked on the gear icon √");
             await driver.findElement(By.linkText('Message Management')).click();
             console.log("5. Selected 'Message Management' √");
             await driver.findElement(By.className('k-pager-info k-label'));
-            await driver.sleep(1500); // Wait for page to load
+            await driver.sleep(4000); // Wait for page to load
             console.log("6. Waited a couple seconds");
             await driver.getTitle().then(function(title) {
                 assert.equal(title, "Message Management - Pharmacy Portal - mosgood");
                 console.log("Title for Message Managment is: " + title );
-            });
-        //});
+                });
+            //});
+          }
+          catch(err) {
+            console.log(err);
+          }
+          finally {
+            console.log("TEST COMPLETED");
+            await driver.quit()
+          }
+
     });
 });
