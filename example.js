72d5855c6fdb430db881bbf962894a84b4525844
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-03 13:54:00 -0800

    exporting example.js to test.js

--- example.js
+++ example.js
@@ -2,15 +2,17 @@ const {Builder, By, Key, until} = require('selenium-webdriver');
 let credentials = require('./credentials.js');
 let environment = require('./environment.js');
 
-async function example() {
-  let driver = await new Builder()
-  .forBrowser('chrome')
-  .build();
-
-  await driver.get(environment.prod)
-  await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user)
-  await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN)
-  await driver.findElement(By.id('pwTopGearIcon')).click()
-  await takeScreenshot()
-}
+const example = async function() {
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+      await driver.get(environment.prod);
+      await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.voiceport_user);
+      await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.voiceport_password, Key.RETURN);
+      await driver.findElement(By.id('pwTopGearIcon')).click();
+  } 
+  catch(err) {
+      console.log(err);
+  }
+};
 example();
+module.exports = example;
