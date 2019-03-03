6af174fef62fc0aab4d00c87aa4b139350916021
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-03 15:22:56 -0800

    updated sample test

---
+++ google_prod_sample.js
@@ -0,0 +1,30 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let environment = require('./environment.js');
+let assert = require("chai").assert;
+
+(async function SignIn() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        console.log("Initiating Login Test in Chrome");
+        await driver.get(environment.prod);
+        console.log("1. Went to Production: √");
+        await driver.manage().window().maximize();
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+        console.log("2. Entered internal username: √");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
+        console.log("3. Entered password and clicked 'Enter': √");
+        await driver.getTitle().then(function(title) {
+            assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
+            console.log("Asserted title is: " + title );
+        });
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("4. Clicked on the gear icon: √");
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("TEST RESULTS: Internal user successfully signed into Production with Chrome");
+    }
+})();
