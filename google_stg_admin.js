d66b7ee729bb2124025ec83f61414fa66b8aab9c
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-17 18:39:03 -0700

    uncommendted out the ondemand section

--- google_stg_admin.js
+++ google_stg_admin.js
@@ -123,27 +123,6 @@ let assert = require("chai").assert;
             console.log("Asserted title is: " + title );
         });
 
-        // Send OnDemand via gear icon
-        /* 
-        await driver.findElement(By.id('pwTopGearIcon')).click();
-        console.log("Clicked on the gear icon: √");
-        await driver.findElement(By.linkText('OnDemand')).click();
-        console.log("Selected 'OnDemand' √");
-        await driver.sleep(6000); // Wait for page to load
-        await driver.findElement(By.id('phone_numbers')).sendKeys('9494158370');
-        console.log("Entered a phone number √");
-        await driver.sleep(6000); // Wait for page to load
-        await driver.findElement(By.css('.btn-pw')).click();
-        console.log("Clicked on 'Next' button");
-        await driver.sleep(6000); // Wait for page to load
-        await driver.findElement(By.css('.btn-select-campaign')).click();
-        console.log("Clicked on 'Select' button");
-        await driver.sleep(6000); // Wait for page to load
-        await driver.findElement(By.css('.fright')).click();
-        console.log("Clicked on 'Send Message' button");
-        await driver.sleep(6000); // Wait for page to load
-        */
-
         // Verify Title for Clinical Calendar
         await driver.findElement(By.className('fa-calendar')).click();
         console.log("Clicked on the calendar icon √");
@@ -214,6 +193,26 @@ let assert = require("chai").assert;
             assert.equal(title, "PrescribeCare: Patients with Encounters - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
             console.log("Asserted title for 'PrescribeCare' page is: " + title );
         });
+
+        // Send OnDemand via gear icon 
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon: √");
+        await driver.findElement(By.linkText('OnDemand')).click();
+        console.log("Selected 'OnDemand' √");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.id('phone_numbers')).sendKeys('9494158370');
+        console.log("Entered a phone number √");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.css('.btn-pw')).click();
+        console.log("Clicked on 'Next' button");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.css('.btn-select-campaign')).click();
+        console.log("Clicked on 'Select' button");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.css('.fright')).click();
+        console.log("Clicked on 'Send Message' button");
+        await driver.sleep(6000); // Wait for page to load
+
         
     }
     catch(err) {
