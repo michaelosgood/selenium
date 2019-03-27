f34ecb5c871f1b418b8358b2595aa5b7a3f0a1fd
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-27 09:51:38 -0700

    updated dev sign in for chain

--- firefox_dev_developer_signin.js
+++ firefox_dev_developer_signin.js
@@ -4,11 +4,15 @@ let environment = require('./environment.js');
 
 (async function SignIn() {
   let driver = await new Builder().forBrowser('firefox').build();
-    try {
-        await driver.get(environment.dev);
-        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.dev_user);
-        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.dev_password, Key.RETURN);
-        await driver.findElement(By.id('pwTopGearIcon')).click();
+  try {
+    await driver.get(environment.dev);
+    console.log("Went to DEV");
+    await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.dev_user);
+    console.log("Entered username");
+    await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.dev_password, Key.RETURN);
+    console.log("Entered password and pressed 'Enter'");
+    await driver.get(environment.chain);
+    console.log("Went to Chain CDN");
     } 
     catch(err) {
         console.log(err);
