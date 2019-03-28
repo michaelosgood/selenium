d0341be9d405f2db0091f9ecf8a068eec9aa4cfb
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-28 14:06:38 -0700

    updated test_suite.js - has boiler plate for mocha test

--- test_suite.js
+++ test_suite.js
@@ -1,52 +1,71 @@
-describe("Inner Suite 1", function(){
-  
-    before(function(){
-        
-        // do something before test suite execution
-        // no matter if there are failed cases
+const {Builder, By, Key, actions, until} = require('selenium-webdriver');
+let credentials = require('./credentials.js');
+let environment = require('./environment.js');
+let assert = require("chai").assert;
+// let driver = new Builder().forBrowser('chrome').build();
+
+
+describe("Inner Suite 1", async function(){
     
+    before(async function(){
+        try {
+            console.log("BEFORE");
+        } catch(error) {
+            console.error(error);
+        }
     });
- 
-    after(function(){
- 
-        // do something after test suite execution is finished
-        // no matter if there are failed cases
- 
+
+    after(async function(){
+        try {
+            console.log("AFTER EACH");
+        } catch(error) {
+            console.error(error);
+        }
+        
     });
     
-    beforeEach(function(){
-        
-        // do something before test case execution
-        // no matter if there are failed cases
     
+    beforeEach(async function(){
+        try {
+            console.log("BEFORE EACH");
+        } catch(error) {
+            console.error(error);
+        }
+    });
+
+    afterEach(async function(){
+        try {
+            console.log("AFTER");
+        } catch(error) {
+            console.error(error);
+        }
+    });
+
+    it("Test-1", async function(){
+        try {
+            console.log("TEST 1");
+         
+        } catch(error) {
+            console.error(error);
+        }
+    });
+
+    it("Test-2", async function(){  
+        try {
+            console.log("TEST 2");
+         
+        } catch(error) {
+            console.error(error);
+        }
     });
  
-    afterEach(function(){
- 
-        // do something after test case execution is finished
-        // no matter if there are failed cases
- 
+    it("Test-3", async function(){  
+        try {
+            console.log("TEST 3");
+         
+        } catch(error) {
+            console.error(error);
+        }
     });
-  
-    it("Test-1", function(){
-        
-        // test Code
-        // assertions
- 
-    });
- 
-    it("Test-2", function(){
- 
-        // test Code
-        // assertions
-        
-    });
- 
-    it("Test-3", function(){
- 
-        // test Code
-        // assertions
- 
-    });
-  
+
 });
