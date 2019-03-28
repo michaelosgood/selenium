8f888fc11bef3c4c6671d9b826f47981e8cdcfbf
Michael Osgood <michael.f.osgood@gmail.com>
2019-03-28 13:46:34 -0700

    making progress on smoketest.js execution

--- smoketest.js
+++ smoketest.js
@@ -2,12 +2,17 @@ const {Builder, By, Key, actions, until} = require('selenium-webdriver');
 let credentials = require('./credentials.js');
 let environment = require('./environment.js');
 let assert = require("chai").assert;
+let driver = new Builder().forBrowser('chrome').build();
 
 
 describe("Inner Suite 1", async function(){
-  
+    
     before(async function(){
         try {
+            console.log("BEFORE");
+            console.log("Starting Smoke Test in Chrome");
+            await driver.get(environment.stg);
+            console.log("Went to Staging √");
             await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
             console.log("Entered internal username √");
             await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
@@ -18,6 +23,7 @@ describe("Inner Suite 1", async function(){
  
     after(async function(){
         try {
+            console.log("AFTER");
             await driver.findElement(By.id('pwTopGearIcon')).click();
             console.log("Clicked on the gear icon √");
             await driver.findElement(By.linkText('Logout')).click();
@@ -28,11 +34,11 @@ describe("Inner Suite 1", async function(){
     });
     
     beforeEach(async function(){
-        let driver = await new Builder().forBrowser('chrome').build();
         try {
-            console.log("Starting Smoke Test in Chrome");
-            await driver.get(environment.stg);
-            console.log("Went to Staging √");
+            console.log("BEFORE EACH");
+            await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+            console.log("Entered internal username √");
+            await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
         } catch(error) {
             console.error(error);
         }
@@ -40,6 +46,7 @@ describe("Inner Suite 1", async function(){
  
     afterEach(async function(){
         try {
+            console.log("AFTER EACH");
             console.log("Driver Quit");
         await driver.quit()
         } catch(error) {
@@ -49,6 +56,7 @@ describe("Inner Suite 1", async function(){
   
     it("Test-1", async function(){
         try {
+            console.log("TEST 1");
             await driver.findElement(By.id('pwTopGearIcon')).click();
             console.log("Clicked on the gear icon √");
             await driver.findElement(By.linkText('Partner Management')).click();
@@ -63,23 +71,24 @@ describe("Inner Suite 1", async function(){
         } catch(error) {
             console.error(error);
         }
- 
     });
  
-    it("Test-2", async function(){  
-        try {
-        console.log("test 2");
-        } catch(error) {
-            console.error(error);
-        }
-    });
+    // it("Test-2", async function(done){  
+    //     try {
+    //     console.log("test 2");
+    //     } catch(error) {
+    //         console.error(error);
+    //     }
+    //     done();
+    // });
  
-    it("Test-3", async function(){  
-        try {
-            console.log("test 3");
-        } catch(error) {
-            console.error(error);
-        }
-    });
-  
+    // it("Test-3", async function(){  
+    //     try {
+    //         console.log("test 3");
+    //     } catch(error) {
+    //         console.error(error);
+    //     }
+    //     done();
+    // });
+
 });
