f4f3835c7f4d13337a1fe6cb5b0880d187426899
Michael Osgood <michael.f.osgood@gmail.com>
2019-04-02 14:56:46 -0700

    updted mosgood and experimenting with diff frameworks

--- mosgood.js
+++ mosgood.js
@@ -1,16 +1,32 @@
 const {Builder, By, Key, until} = require('selenium-webdriver');
 let credentials = require('./credentials.js');
 let environment = require('./environment.js');
+let assert = require("chai").assert;
+const currentDate = Date.now().toString();
 
-async function mosgood() {
-  let driver = await new Builder()
-  .forBrowser('chrome')
-  .build();
-
-  await driver.get(environment.stg)
-  await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user)
-  await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN)
-  await driver.findElement(By.id('pwTopGearIcon')).click()
-  // await driver.findElement(By.linkText('Logout')).click()
-}
-mosgood();
+const mosgood = async function() {
+  console.log(currentDate);
+  let driver = await new Builder().forBrowser('chrome').build();
+  try {
+    // Login to Admin User and Verify Title
+    console.log("Starting Admin Test in Firefox");
+    await driver.get(environment.stg);
+    console.log("Went to STG");
+    await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+    console.log("Entered internal username");
+    await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
+    console.log("Entered password and clicked 'Enter'");
+    await driver.sleep(6000); // Wait for page to load
+    await driver.getTitle().then(function(title) {
+      assert.equal(title, "Pharmacy Group Dashboard - Pharmacy Portal - mosgood");
+      console.log("Asserted title is: " + title );
+    });
+  } 
+  catch(err) {
+      console.log(err);
+  }
+  finally {
+    console.log("Test Completed!!");
+    await driver.quit()
+  }
+}();
