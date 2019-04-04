ee8c1aa9df54fb13aee436658c3090cf07595f2d
Michael Osgood <michael.f.osgood@gmail.com>
2019-04-04 16:32:19 -0700

    updated ad rings test

--- google_stg_ad_rings.js
+++ google_stg_ad_rings.js
@@ -13,6 +13,16 @@ let assert = require("chai").assert;
         console.log("2. Entered internal username");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.sample_password, Key.RETURN);
         console.log("3. Entered password and clicked 'Enter'");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.linkText('Active Patients')).click();
+        console.log("4. Clicked on 'Active Patients'");
+        await driver.sleep(6000); // Wait for page to load
+        // Tried finding by xpath. not working 
+        // await driver.findElement(By.xpath('//*[@id="chart_diabetic_statin_pie"]/div/div[1]/div/svg/g[1]/path')).click();
+        
+        //Tried finding by css - not working
+        // await driver.findElement(By.css("#chart_diabetic_statin_pie > div > div:nth-child(1) > div > svg > g:nth-child(2) > path")).click();
+       
     }
     catch(err) {
         console.log(err);
