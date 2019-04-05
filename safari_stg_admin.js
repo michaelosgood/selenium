40c75e2919fd76e85659ed6e50ad1f9f18eee195
Michael Osgood <michael.f.osgood@gmail.com>
2019-04-05 10:53:45 -0700

    slowed down the safari test to allow pages to load

--- safari_stg_admin.js
+++ safari_stg_admin.js
@@ -8,19 +8,19 @@ let assert = require("chai").assert;
     try {
         console.log("Initiating Admin Test in Safari");
         await driver.get(environment.stg);
-        console.log("1. Went to Staging √");
+        console.log("1. Went to Staging");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
-        console.log("2. Entered internal username √");
+        console.log("2. Entered internal username");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
         console.log("3. Entered password and clicked 'Enter' √");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         // Message Management Test
         await driver.findElement(By.id('pwTopGearIcon')).click();
         console.log("4. Clicked on the gear icon √");
         await driver.findElement(By.linkText('Message Management')).click();
         console.log("5. Selected 'Message Management' √");
         await driver.findElement(By.className('k-pager-info k-label'));
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("6. Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Message Management - Pharmacy Portal - mosgood");
@@ -32,7 +32,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('Campaign Management')).click();
         console.log("8. Selected 'Campaign Managment' √");
         await driver.findElement(By.className('k-pager-info k-label'));
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("9. Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Campaign Management - Pharmacy Portal - mosgood");
@@ -44,7 +44,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('Login As')).click();
         console.log("11. Selected 'Login As' √");
         await driver.findElement(By.className('k-pager-info k-label'));
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("12. Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Login As - Pharmacy Portal - mosgood");
@@ -56,7 +56,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('Template Management')).click();
         console.log("14. Selected 'Template Management' √");
         await driver.findElement(By.className('k-pager-info k-label'));
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("15. Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Communication Templates - Pharmacy Portal - mosgood");
@@ -68,7 +68,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('User Management')).click();
         console.log("17. Selected 'User Management' √");
         await driver.findElement(By.className('k-pager-info k-label'));
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("18. Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "User Management - Pharmacy Portal - mosgood");
@@ -80,7 +80,7 @@ let assert = require("chai").assert;
          await driver.findElement(By.linkText('Account Management')).click();
          console.log("20. Selected 'Account Management' √");
          await driver.findElement(By.className('k-pager-info k-label'));
-         await driver.sleep(6000); // Wait for page to load
+         await driver.sleep(10000); // Wait for page to load
          console.log("21. Waited a couple seconds");
          await driver.getTitle().then(function(title) {
              assert.equal(title, "Account Management - Pharmacy Portal - mosgood");
@@ -92,7 +92,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('Partner Management')).click();
         console.log("23. Selected 'Partner Management' √");
         await driver.findElement(By.className('k-pager-info k-label'));
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("24. Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Partner Management - Pharmacy Portal - mosgood");
@@ -102,6 +102,7 @@ let assert = require("chai").assert;
          // Logout of Admin
         await driver.findElement(By.id('pwTopGearIcon')).click();
         console.log("Clicked on the gear icon");
+        await driver.sleep(10000); // Wait for page to load
         await driver.findElement(By.linkText('Logout')).click();
         console.log("Selected 'Logout'");
 
@@ -111,7 +112,7 @@ let assert = require("chai").assert;
         console.log("Entered username");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
         console.log("Entered password and clicked 'Enter'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -121,7 +122,7 @@ let assert = require("chai").assert;
         // Verify Title for Active Patients page
         await driver.findElement(By.linkText('Active Patients')).click();
         console.log("Clicked on 'Active Patients'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
             console.log("Asserted title is: " + title );
@@ -130,7 +131,7 @@ let assert = require("chai").assert;
         // Verify Title for Drugs page
         await driver.findElement(By.linkText('Drugs')).click();
         console.log("Clicked on 'Drugs'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
             console.log("Asserted title is: " + title );
@@ -139,7 +140,7 @@ let assert = require("chai").assert;
         // Verify Title for Diseases page
         await driver.findElement(By.linkText('Diseases')).click();
         console.log("Clicked on 'Diseases'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
             console.log("Asserted title is: " + title );
@@ -148,7 +149,7 @@ let assert = require("chai").assert;
         // Verify Title for Physicians page
         await driver.findElement(By.linkText('Physicians')).click();
         console.log("Clicked on 'Physicians'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
             console.log("Asserted title is: " + title );
@@ -157,7 +158,7 @@ let assert = require("chai").assert;
         // Verify Title for Physician Groups page
         await driver.findElement(By.linkText('Physician Groups')).click();
         console.log("Clicked on 'Physician Groups'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
             console.log("Asserted title is: " + title );
@@ -168,7 +169,7 @@ let assert = require("chai").assert;
         console.log("Clicked on the calendar icon");
         await driver.findElement(By.linkText('Clinical Calendar')).click();
         console.log("Selected 'Clinical Calendar' icon");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Calendar - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -179,7 +180,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.className('fa-calendar')).click();
         console.log("Clicked on the calendar icon");
         await driver.findElement(By.linkText('Social Calendar')).click();
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         console.log("Selected 'Social Calendar' icon");
         await driver.getTitle().then(function(title) {
@@ -191,7 +192,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.className('fa-calendar')).click();
         console.log("Clicked on the calendar icon √");
         await driver.findElement(By.linkText('Custom Calendar')).click();
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         console.log("Selected 'Custom Calendar' icon");
         await driver.getTitle().then(function(title) {
@@ -202,7 +203,7 @@ let assert = require("chai").assert;
         // Verify Title for 'Growth' Pt List
         await driver.findElement(By.linkText('Growth')).click();
         console.log("Selected 'Growth'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Growth: Patients - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -213,7 +214,7 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('StarWellness')).click();
         console.log("Selected 'StarWellness'");
         await driver.findElement(By.linkText('Synchronization')).click();
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "StarWellness: Synchronization - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -223,7 +224,7 @@ let assert = require("chai").assert;
         // Verify Title for 'VaccineComplete' Pt List
         await driver.findElement(By.linkText('VaccineComplete')).click();
         console.log("Selected 'VaccineComplete'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Vaccine Patient List - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -233,7 +234,7 @@ let assert = require("chai").assert;
         // Verify Title for 'PrescribeMedicare' Pt List
         await driver.findElement(By.linkText('PrescribeMedicare')).click();
         console.log("Selected 'PrescribeMedicare'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Medicare Patient List - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -243,7 +244,7 @@ let assert = require("chai").assert;
         // Verify Title for 'PrescribeCare' Pt List
         await driver.findElement(By.linkText('PrescribeCare')).click();
         console.log("Selected 'PrescribeCare'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         console.log("Waited a couple seconds");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "PrescribeCare: Patients with Encounters - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - H3N2");
@@ -255,19 +256,19 @@ let assert = require("chai").assert;
         console.log("Clicked on the gear icon");
         await driver.findElement(By.linkText('OnDemand')).click();
         console.log("Selected 'OnDemand'");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.findElement(By.id('phone_numbers')).sendKeys('3036534679');
         console.log("Entered a phone number");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.findElement(By.css('.btn-pw')).click();
         console.log("Clicked on 'Next' button");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.findElement(By.css('.btn-select-campaign')).click();
         console.log("Clicked on 'Select' button");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
         await driver.findElement(By.css('.fright')).click();
         console.log("Clicked on 'Send Message' button");
-        await driver.sleep(6000); // Wait for page to load
+        await driver.sleep(10000); // Wait for page to load
 
     } 
     catch(err) {
