2edaf0fc97ad8ce33a3884b372f70f0f3cdc4067
Michael Osgood <michael.f.osgood@gmail.com>
2019-04-30 19:59:18 -0700

    added prod smoketests for chrome and firefox

---
+++ test/prod-test.js
@@ -0,0 +1,11 @@
+let googleProdSmoketest = require('../pec-tests/google_prod_smoketest');
+let firefoxProdSmoketest = require('../pec-tests/firefox_prod_smoketest');
+
+(async function stgSmoketest() { 
+    try {
+        await googleProdSmoketest();
+        await firefoxProdSmoketest();
+    } catch (error) {
+        throw error;
+    }
+})();
