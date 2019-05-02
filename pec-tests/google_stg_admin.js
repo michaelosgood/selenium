9ebf1603d1e86af83ec7394cc33ef60d9ade5209
Michael Osgood <michael.f.osgood@gmail.com>
2019-05-02 15:23:35 -0700

    added timing after sending custom ondemand

--- pec-tests/google_stg_admin.js
+++ pec-tests/google_stg_admin.js
@@ -248,34 +248,6 @@ let assert = require("chai").assert;
             console.log("Asserted title for 'PrescribeCare' page is: " + title);
         });
 
-        // Send Custom OnDemand SMS via gear icon 
-        await driver.sleep(6000); // Wait for page to load
-        await driver.findElement(By.id('pwTopGearIcon')).click();
-        console.log("Clicked on the gear icon: √");
-        await driver.findElement(By.linkText('OnDemand')).click();
-        console.log("Selected 'OnDemand' √");
-        await driver.sleep(3000); // Wait for page to load
-        await driver.findElement(By.id('phone_numbers')).sendKeys('9494158370');
-        console.log("Entered a phone number √");
-        await driver.sleep(3000); // Wait for page to load
-        await driver.findElement(By.css('.btn-pw')).click();
-        console.log("Clicked on 'Next' button");
-        await driver.sleep(3000); // Wait for page to load
-        await driver.findElement(By.css('.create-new-sms-btn')).click();
-        console.log("Clicked on '+ Create New SMS' button");
-        await driver.sleep(3000); // Wait for page to load
-        await driver.findElement(By.className('form-control sms')).sendKeys('QA Automated Test - Create and send SMS');
-        console.log("Entered SMS content in text area");
-        await driver.findElement(By.className('btn btn-pw fright')).click();
-        console.log("Clicked on 'Create' button");
-        await driver.sleep(3000); // Wait for page to load
-        await driver.findElement(By.className('btn btn-pw btn-select-campaign')).click();
-        console.log("Clicked on 'Select' button");
-        await driver.sleep(3000); // Wait for page to load
-        await driver.findElement(By.css('.fright')).click();
-        console.log("Clicked on 'Send Message' button");
-        await driver.sleep(6000); // Wait for page to load
-
         // Use magnifying glass to search for 'Allen' and click on name
         await driver.findElement(By.className('fa-search')).click();
         console.log("Clicked on the search icon");
@@ -337,6 +309,34 @@ let assert = require("chai").assert;
         await driver.findElement(By.linkText('Log')).click();
         console.log("Clicked on Log tab");
         await driver.sleep(6000); // wait for page to load
+
+        // Send Custom OnDemand SMS via gear icon 
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon: √");
+        await driver.findElement(By.linkText('OnDemand')).click();
+        console.log("Selected 'OnDemand' √");
+        await driver.sleep(3000); // Wait for page to load
+        await driver.findElement(By.id('phone_numbers')).sendKeys('9494158370');
+        console.log("Entered a phone number √");
+        await driver.sleep(3000); // Wait for page to load
+        await driver.findElement(By.css('.btn-pw')).click();
+        console.log("Clicked on 'Next' button");
+        await driver.sleep(3000); // Wait for page to load
+        await driver.findElement(By.css('.create-new-sms-btn')).click();
+        console.log("Clicked on '+ Create New SMS' button");
+        await driver.sleep(3000); // Wait for page to load
+        await driver.findElement(By.className('form-control sms')).sendKeys('QA Automated Test - Create and send SMS');
+        console.log("Entered SMS content in text area");
+        await driver.findElement(By.className('btn btn-pw fright')).click();
+        console.log("Clicked on 'Create' button");
+        await driver.sleep(3000); // Wait for page to load
+        await driver.findElement(By.className('btn btn-pw btn-select-campaign')).click();
+        console.log("Clicked on 'Select' button");
+        await driver.sleep(3000); // Wait for page to load
+        await driver.findElement(By.css('.fright')).click();
+        console.log("Clicked on 'Send Message' button");
+        await driver.sleep(10000); // Wait for page to load
 
     }
     catch (err) {
