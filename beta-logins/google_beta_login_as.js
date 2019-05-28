0d1717536cb8df37575b10b618306d7dacd53239
Michael Osgood <michael.f.osgood@gmail.com>
2019-05-28 10:12:50 -0700

    added login as for beta

---
+++ beta-logins/google_beta_login_as.js
@@ -0,0 +1,34 @@
+const {Builder, By, Key, actions, until} = require('selenium-webdriver');
+let credentials = require('../credentials.js');
+let environment = require('../environment.js');
+let assert = require("chai").assert;
+
+(async function loginAs() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        console.log("Initiating Login Test in Chrome");
+        await driver.get(environment.beta);
+        console.log("Went to Beta");
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+        console.log("Entered internal username");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
+        console.log("Entered password and clicked 'Enter'");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Login As')).click();
+        console.log("Selected 'Login As'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function(title) {
+            assert.equal(title, "Login As - Pharmacy Portal - mosgood");
+            console.log("Title for 'Login As' is: " + title );
+        });
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("User has been taken to 'Login As'");
+    }
+})();
