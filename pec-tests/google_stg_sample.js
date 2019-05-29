86ad4bceb452d91b6e6fda0823912a2eb8e215e4
Michael Osgood <michael.f.osgood@gmail.com>
2019-05-29 10:13:23 -0700

    added updated stg sample test

--- pec-tests/google_stg_sample.js
+++ pec-tests/google_stg_sample.js
@@ -8,17 +8,17 @@ let assert = require("chai").assert;
     try {
         console.log("Initiating Login Test in Chrome");
         await driver.get(environment.stg);
-        console.log("1. Went to Staging √");
+        console.log("Went to Staging");
         await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.sample_user);
-        console.log("2. Entered internal username √");
+        console.log("Entered internal username");
         await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.sample_password, Key.RETURN);
-        console.log("3. Entered password and clicked 'Enter' √");
+        console.log("Entered password and clicked 'Enter'");
         await driver.getTitle().then(function(title) {
             assert.equal(title, "Pharmacy Dashboard - Pharmacy Portal - Account: 86362 - NPI: 1234934850 - NCPDP: 8888888 - SampUser");
             console.log("Asserted title is: " + title );
         });
         await driver.findElement(By.className('fa-calendar')).click();
-        console.log("4. Clicked on the calendar icon √");
+        console.log("Clicked on the calendar icon");
         await driver.findElement(By.linkText('Clinical Calendar')).click();
     } 
     catch(err) {
