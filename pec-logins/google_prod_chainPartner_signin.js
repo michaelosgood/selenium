f4a1d1fcee6430e79890b5a44530fa17ed70f350
Michael Osgood <michael.f.osgood@gmail.com>
2019-08-07 14:43:31 -0700

    addded chain sample and chain partner signin scripts

---
+++ pec-logins/google_prod_chainPartner_signin.js
@@ -0,0 +1,22 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('../credentials.js');
+let environment = require('../environment.js');
+
+(async function chainPartnerSignIn() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        console.log("Initiating Login Test in Chrome");
+        await driver.get(environment.prod);
+        console.log("Went to Prod");
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.chainPartner_user);
+        console.log("Entered username");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.chainPartner_password, Key.RETURN);
+        console.log("Entered password and clicked 'Enter'");
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("Chain Partner user signed in with Chrome");
+    }
+})();
