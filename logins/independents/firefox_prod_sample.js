3499d3fe23ba11784123d403bfcdd276b12c563a
Michael Osgood <michael.f.osgood@gmail.com>
2019-10-01 20:46:10 -0700

    added firefox scripts

---
+++ logins/independents/firefox_prod_sample.js
@@ -0,0 +1,27 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('../../credentials.js');
+let environment = require('../../environment.js');
+let assert = require("chai").assert;
+
+(async function SignIn() {
+  let driver = await new Builder().forBrowser('firefox').build();
+    try {
+        console.log("Initiating Login Test in Firefox");
+        await driver.get(environment.prod);
+        console.log("Went to Production");
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.sample_user);
+        console.log("Entered internal username");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.sample_password, Key.RETURN);
+        console.log("Entered password and clicked 'Enter'");
+        await driver.sleep(10000);
+        //click on close button in merck modal
+        await driver.findElement(By.xpath("//button[@class = 'btn btn-default']")).click();
+        console.log("Closed out Merck Modal");
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("Sample user signed into Production with Firefox");
+    }
+})();
---
+++ logins/independents/firefox_prod_voiceport_signin.js
@@ -0,0 +1,25 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('../../credentials.js');
+let environment = require('../../environment.js');
+
+(async function VoiceportSignIn() {
+  let driver = await new Builder().forBrowser('firefox').build();
+    try {
+        console.log("Initiating VoicePort Sign In Test in Firefox");
+        await driver.get(environment.prod);
+        console.log("Went to PROD");
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.voiceport_user);
+        console.log("Entered Voiceport username");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.voiceport_password, Key.RETURN);
+        console.log("Entered password and clicked 'Enter'");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("Voiceport user signed into PROD with Firefox");
+    }
+})();
