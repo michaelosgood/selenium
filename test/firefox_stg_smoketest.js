f1997f7a65879cd6ea789077e9d575d5c345f744
Michael Osgood <michael.f.osgood@gmail.com>
2019-10-09 16:46:16 -0700

    updated order of firefox smoketest

--- test/firefox_stg_smoketest.js
+++ test/firefox_stg_smoketest.js
@@ -1,10 +1,11 @@
-let firefoxStgIndependents = require('../pec-tests/firefox_stg_independents.js');
 let firefoxStgAdmins = require('../pec-tests/firefox_stg_admins.js');
+let firefoxStgIndependents = require('../pec-tests/firefox_stg_independents.js');
+
 
 (async function stgSmoketest() { 
     try {
-        await firefoxStgIndependents();
         await firefoxstgAdmins();
+        await firefoxStgIndependents();
     } catch (error) {
         throw error;
     }
