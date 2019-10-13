4a71bd62c73e87796600341329a573bce0bee4f0
Michael Osgood <michael.f.osgood@gmail.com>
2019-10-13 10:53:05 -0700

    added homepages to my smoketest

--- test/firefox_stg_smoketest.js
+++ test/firefox_stg_smoketest.js
@@ -1,11 +1,13 @@
 let firefoxStgAdmins = require('../pec-tests/firefox_stg_admins.js');
 let firefoxStgIndependents = require('../pec-tests/firefox_stg_independents.js');
+let firefoxStgPtProfiles = require('../pec-tests/firefox_stg_pt_profiles.js');
 
 
 (async function stgSmoketest() { 
     try {
-        await firefoxStgAdmins();
-        await firefoxStgIndependents();
+        //await firefoxStgAdmins();
+       // await firefoxStgIndependents();
+        await firefoxStgPtProfiles();
     } catch (error) {
         throw error;
     }
