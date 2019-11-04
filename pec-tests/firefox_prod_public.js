69fdd7af0d7ea01824967303519effcb2931e278
Michael Osgood <michael.f.osgood@gmail.com>
2019-11-04 15:16:19 -0800

    added firefox public site to smoketst

--- pec-tests/firefox_prod_public.js
+++ pec-tests/firefox_prod_public.js
@@ -3,13 +3,13 @@ let credentials = require('../credentials.js');
 let environment = require('../environment.js');
 let assert = require("chai").assert;
 
-const firefoxStgPublic = async function() {
+const firefoxProdPublic = async function() {
     let driver = await new Builder().forBrowser('firefox').build();
         try {
             // Go to Publis Site and Verify Title
             console.log("Starting Staging Public Site in Firefox");
-            await driver.get(environment.stg_public);
-            console.log("Went to Staging's Public Site");
+            await driver.get(environment.prod_public);
+            console.log("Went to Production's Public Site");
             await driver.sleep(6000); // Wait for page to load
             await driver.getTitle().then(function (title) {
                 assert.equal(title, "Home | PrescribeWellness");
@@ -26,4 +26,4 @@ const firefoxStgPublic = async function() {
         }
     };
 
-module.exports = firefoxStgPublic;
+module.exports = firefoxProdPublic;
--- test-runner/firefox_prod_smoketest.js
+++ test-runner/firefox_prod_smoketest.js
@@ -1,10 +1,12 @@
 let firefoxProdIndependents = require('../pec-tests/firefox_prod_independents.js');
 let firefoxProdAdmins = require('../pec-tests/firefox_prod_admins.js');
+let firefoxProdPublic = require('../pec-tests/firefox_prod_public.js');
 
 (async function prodSmoketest() { 
     try {
         await firefoxProdIndependents();
         await firefoxProdAdmins();
+        await firefoxProdPublic();
     } catch (error) {
         throw error;
     }
