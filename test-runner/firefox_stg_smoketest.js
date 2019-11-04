04aa4206cd81fafef1677df51b35fb736da98839
Michael Osgood <michael.f.osgood@gmail.com>
2019-11-04 15:01:08 -0800

    added public site test to smoketest

--- test-runner/firefox_stg_smoketest.js
+++ test-runner/firefox_stg_smoketest.js
@@ -1,13 +1,15 @@
 let firefoxStgAdmins = require('../pec-tests/firefox_stg_admins.js');
 let firefoxStgIndependents = require('../pec-tests/firefox_stg_independents.js');
 let firefoxStgPtProfiles = require('../pec-tests/firefox_stg_pt_profiles.js');
+let firefoxStgPublic = require('../pec-tests/firefox_stg_public.js');
 
 
 (async function stgSmoketest() { 
     try {
-        //await firefoxStgAdmins();
-       // await firefoxStgIndependents();
+        await firefoxStgAdmins();
+        await firefoxStgIndependents();
         await firefoxStgPtProfiles();
+        await firefoxStgPublic()
     } catch (error) {
         throw error;
     }
--- test-runner/google_stg_smoketest.js
+++ test-runner/google_stg_smoketest.js
@@ -1,12 +1,14 @@
 let googleStgIndependents = require('../pec-tests/google_stg_independents.js');
 let googleStgAdmins = require('../pec-tests/google_stg_admins.js');
 let googleStgPtProfiles = require('../pec-tests/google_stg_patient_profiles.js');
+let googleStgPublic = requre('../pec-tests/google_stg_public.js');
 
 (async function stgSmoketest() { 
     try {
         await googleStgAdmins();
         await googleStgIndependents();
         await googleStgPtProfiles();
+        await googleStgPublic();
     } catch (error) {
         throw error;
     }
