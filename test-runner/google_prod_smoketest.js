1f689bd52e1994bb97b48af9632a5484566e2a94
Michael Osgood <michael.f.osgood@gmail.com>
2019-11-04 14:51:30 -0800

    added a public site test

--- test-runner/google_prod_smoketest.js
+++ test-runner/google_prod_smoketest.js
@@ -1,12 +1,14 @@
 let googleProdIndependents = require('../pec-tests/google_prod_independents.js');
 let googleProdAdmins = require('../pec-tests/google_prod_admins.js');
 let googleProdPtProfiles = require('../pec-tests/google_prod_patient_profiles.js');
+let googleProdPublic = require('../pec-tests/google_prod_public.js');
 
 (async function prodSmoketest() { 
     try {
         await googleProdAdmins();
         await googleProdIndependents();
         await googleProdPtProfiles();
+        await googleProdPublic();
     } catch (error) {
         throw error;
     }
