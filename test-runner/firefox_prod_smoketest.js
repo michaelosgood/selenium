f4c347ae16da10a9b1cf604b0af422372e05e684
Michael Osgood <michael.f.osgood@gmail.com>
2019-11-06 20:18:16 -0800

    updaetd firefox smoketest

--- test-runner/firefox_prod_smoketest.js
+++ test-runner/firefox_prod_smoketest.js
@@ -1,11 +1,13 @@
-let firefoxProdIndependents = require('../pec-tests/firefox_prod_independents.js');
 let firefoxProdAdmins = require('../pec-tests/firefox_prod_admins.js');
+let firefoxProdIndependents = require('../pec-tests/firefox_prod_independents.js');
+let firefoxProdPtProfiles = require('../pec-tests/firefox_prod_pt_profiles.js');
 let firefoxProdPublic = require('../pec-tests/firefox_prod_public.js');
 
 (async function prodSmoketest() { 
     try {
-        await firefoxProdIndependents();
         await firefoxProdAdmins();
+        await firefoxProdIndependents();
+        await firefoxProdPtProfiles();
         await firefoxProdPublic();
     } catch (error) {
         throw error;
