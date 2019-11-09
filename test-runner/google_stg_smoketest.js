c9a6c4f3f74984e677634f79da347c75ed737e6e
Michael Osgood <michael.f.osgood@gmail.com>
2019-11-08 17:00:12 -0800

    fixed a typo

--- test-runner/google_stg_smoketest.js
+++ test-runner/google_stg_smoketest.js
@@ -1,7 +1,7 @@
 let googleStgIndependents = require('../tests/google_stg_independents.js');
 let googleStgAdmins = require('../tests/google_stg_admins.js');
 let googleStgPtProfiles = require('../tests/google_stg_patient_profiles.js');
-let googleStgPublic = requre('../tests/google_stg_public.js');
+let googleStgPublic = require('../tests/google_stg_public.js');
 
 (async function stgSmoketest() { 
     try {
