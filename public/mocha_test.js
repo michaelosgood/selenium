79682f57d175820e7bff1b714daaf4a8e7e1663a
Michael Osgood <michael.f.osgood@gmail.com>
2019-11-12 16:52:44 -0800

    fixed mocha example

--- public/mocha_test.js
+++ public/mocha_test.js
@@ -7,7 +7,7 @@ let driver = new Builder().forBrowser('chrome').build();
 describe('Go to Public Site and Verify Title', function() {
 
     it('should navigate to WWW Page', function () {
-      driver.get(environment.public);
+      driver.get(environment.prod_public);
     })
     it('should verify the title displays: "Home | PrescribeWellness"', function() {
       driver.getTitle().then(function(title) {
