a251f69da90404364495a2b30b030ac10d336626
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-17 18:11:47 -0800

    added date to prod smoketest

--- test-runner/google_prod_smoketest.js
+++ test-runner/google_prod_smoketest.js
@@ -3,9 +3,11 @@ let googleProdAdmins = require('../tests/google_prod_admins.js');
 let googleProdPtProfiles = require('../tests/google_prod_patient_profiles.js');
 let googleProdChainPartner = require('../tests/google_prod_chain_partner.js');
 let googleProdPublic = require('../tests/google_prod_public.js');
+let date = new Date();
 
 (async function prodSmoketest() { 
     try {
+        console.log("Smoke Test Started at: " + date);
         await googleProdAdmins();
         await googleProdIndependents();
         await googleProdPtProfiles();
@@ -14,4 +16,7 @@ let googleProdPublic = require('../tests/google_prod_public.js');
     } catch (error) {
         throw error;
     }
+    finally {
+        console.log("Smoke Test Completed at: " + date);
+      }
 })();
