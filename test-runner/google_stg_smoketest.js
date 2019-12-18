29af7b17e12e0ab751e5b9854eb4a41cc4078960
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-17 18:01:41 -0800

    added date stamp to stg smoketest

--- test-runner/google_stg_smoketest.js
+++ test-runner/google_stg_smoketest.js
@@ -3,9 +3,11 @@ let googleStgAdmins = require('../tests/google_stg_admins.js');
 let googleStgPtProfiles = require('../tests/google_stg_patient_profiles.js');
 let googleStgChainPartner = require('../tests/google_stg_chain_partner.js');
 let googleStgPublic = require('../tests/google_stg_public.js');
+let date = new Date();
 
 (async function stgSmoketest() { 
     try {
+        console.log("Smoke Test Started at: " + date);
         await googleStgAdmins();
         await googleStgIndependents();
         await googleStgPtProfiles();
@@ -14,4 +16,7 @@ let googleStgPublic = require('../tests/google_stg_public.js');
     } catch (error) {
         throw error;
     }
+    finally {
+        console.log("Smoke Test Completed at: " + date);
+      }
 })();
