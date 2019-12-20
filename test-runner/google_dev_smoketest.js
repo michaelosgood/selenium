80e6f4b635f06d6f15f0ced851669fb20590e735
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-20 11:17:08 -0800

    added Google Dev smoketest

---
+++ test-runner/google_dev_smoketest.js
@@ -0,0 +1,22 @@
+let googleDevIndependents = require('../tests/google_dev_independents.js');
+let googleDevAdmins = require('../tests/google_dev_admins.js');
+let googleDevPtProfiles = require('../tests/google_dev_patient_profiles.js');
+let googleDevChainPartner = require('../tests/google_dev_chain_partner.js');
+let googleDevPublic = require('../tests/google_dev_public.js');
+let date = new Date();
+
+(async function devSmoketest() { 
+    try {
+        console.log("Smoke Test Started at: " + date);
+        await googleDevAdmins();
+        await googleDevIndependents();
+        await googleDevPtProfiles();
+        await googleDevChainPartner();
+        await googleDevPublic();
+    } catch (error) {
+        throw error;
+    }
+    finally {
+        console.log("Smoke Test Completed at: " + date);
+      }
+})();
---
+++ tests/google_dev_admins.js
@@ -0,0 +1,156 @@
+const { Builder, By, Key, actions, until } = require('selenium-webdriver');
+let credentials = require('../credentials.js');
+let environment = require('../environment.js');
+let titles = require('../components/titles.js');
+let assert = require("chai").assert;
+
+const googleDevAdmins = async function() {
+    let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        // Login to Admin User and Verify Title
+        console.log("Starting Admin Test in Chrome");
+        await driver.get(environment.dev);
+        console.log("Went to Dev");
+        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.internal_user);
+        console.log("Entered internal username");
+        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.internal_password, Key.RETURN);
+        console.log("Entered password and clicked 'Enter'");
+        await driver.sleep(6000); // Wait for page to load
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.dashboard);
+            console.log("Asserted title is: " + title);
+        });
+
+        // Message Management Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Message Management')).click();
+        console.log("Selected 'Message Management'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.msg_mgmt);
+            console.log("Asserted title for Message Managment is: " + title);
+        });
+        // Campaign Management Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Campaign Management')).click();
+        console.log("Selected 'Campaign Managment'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.camp_mgmt);
+            console.log("Asserted title for 'Campaign Managment' page is: " + title);
+        });
+
+        // Login As Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Login As')).click();
+        console.log("Selected 'Login As'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.login_as);
+            console.log("Asserted title for 'Login As' page is: " + title);
+        });
+
+        // Template Managment Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Template Management')).click();
+        console.log("Selected 'Template Management'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.template_mgmt);
+            console.log("Asserted title for 'Template Management' page is: " + title);
+        });
+        // User Managment Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('User Management')).click();
+        console.log("Selected 'User Management'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.user_mgmt);
+            console.log("Asserted title for 'User Management' page is: " + title);
+        });
+        // Account Managment Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Account Management')).click();
+        console.log("Selected 'Account Management'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.account_mgmt);
+            console.log("Asserted title for 'Account Management' page is: " + title);
+        });
+
+        // Partner Managment Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Partner Management')).click();
+        console.log("Selected 'Partner Management'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.partner_mgmt);
+            console.log("Asserted title for 'Partner Management' page is: " + title);
+        });
+
+        // Call History Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Call History')).click();
+        console.log("Selected 'Call History'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.call_history);
+            console.log("Asserted title for 'Call History' page is: " + title);
+        });
+
+        // Developer Tools Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Developer Tools')).click();
+        console.log("Selected 'Developer Tools'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.dev_tools);
+            console.log("Asserted title for 'Developer Tools' page is: " + title);
+        });
+
+        // Settings Verify Title
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.linkText('Settings')).click();
+        console.log("Selected 'Settings'");
+        await driver.sleep(6000); // Wait for page to load
+        console.log("Waited a couple seconds");
+        await driver.getTitle().then(function (title) {
+            assert.equal(title, titles.settings);
+            console.log("Asserted title for 'Settings' page is: " + title);
+        });
+
+        // Logout of Admin
+        await driver.findElement(By.id('pwTopGearIcon')).click();
+        console.log("Clicked on the gear icon");
+        await driver.findElement(By.className('btn-link')).click();
+        console.log("Selected 'Logout'");
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+    finally {
+        console.log("Test Completed!!");
+        await driver.quit()
+    }
+};
+module.exports = googleDevAdmins;
