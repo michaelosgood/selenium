175b95db767cd00aa5d37ad275a615e243592c46
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-24 10:04:45 -0800

    added dev url for public site

---
+++ tests/google_dev_public.js
@@ -0,0 +1,29 @@
+const { Builder, By, Key, actions, until } = require('selenium-webdriver');
+let credentials = require('../credentials.js');
+let environment = require('../environment.js');
+let assert = require("chai").assert;
+
+const googleDevPublic = async function() {
+    let driver = await new Builder().forBrowser('chrome').build();
+        try {
+            // Go to Publis Site and Verify Title
+            console.log("Starting Admin Test in Chrome");
+            await driver.get(environment.dev_public);
+            console.log("Went to DEV's Public Site");
+            await driver.sleep(6000); // Wait for page to load
+            await driver.getTitle().then(function (title) {
+                assert.equal(title, "Home | PrescribeWellness");
+                console.log("Asserted title is: " + title);
+            });
+    
+        }
+        catch (err) {
+            console.log(err);
+        }
+        finally {
+            console.log("Test Completed!!");
+            await driver.quit()
+        }
+    };
+
+module.exports = googleDevPublic;
