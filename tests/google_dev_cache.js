e6907dedd7131dfdde683ff006768ba812c2ebf1
Michael Osgood <michael.f.osgood@gmail.com>
2019-12-31 09:49:27 -0800

    udpaetd cache tests

--- tests/google_dev_cache.js
+++ tests/google_dev_cache.js
@@ -79,6 +79,74 @@ const googleDevCache = async function() {
             console.log("Asserted title for 'PrescribeCare' page is: " + title );
         });
 
+        // Go to Patient Profile
+        await driver.get(pt_profile.dev_link);
+
+        // Click on the Overview tab
+        await driver.findElement(By.linkText('Overview')).click();
+        console.log("Clicked on Overview tab");
+        await driver.sleep(6000); // wait for page to load
+
+        // Click on the Programs tab
+        await driver.findElement(By.linkText('Programs')).click();
+        console.log("Clicked on Programs sub-tab");
+        await driver.sleep(6000); // wait for page to load
+
+        // Click on the 'Medications' tab
+        await driver.findElement(By.linkText('Medications')).click();
+        console.log("Clicked on Medication tab");
+        await driver.sleep(6000); // wait for page to load
+
+        await driver.findElement(By.linkText('Patient-Reported Medication')).click();
+        console.log("Clicked on Patient-Reported Medication sub-tab");
+        await driver.sleep(6000); // wait for page to load
+
+        await driver.findElement(By.linkText('Medication Allergies')).click();
+        console.log("Clicked on Medication Allergies sub-tab");
+        await driver.sleep(6000); // wait for page to load
+
+        await driver.findElement(By.linkText('Medication History')).click();
+        console.log("Clicked on Medication History sub-tab");
+        await driver.sleep(6000); // wait for page to load
+
+        await driver.findElement(By.linkText('Med Time Reminders')).click();
+        console.log("Clicked on Med Time Reminders sub-tab");
+        await driver.sleep(6000); // wait for page to load
+
+        // Click on Medicare tab 
+        await driver.findElement(By.linkText('Medicare')).click();
+        console.log("Clicked on Medicare tab");
+        await driver.sleep(10000); // wait for page to load
+
+        // Click on Vaccines tab
+        await driver.findElement(By.linkText('Vaccines')).click();
+        console.log("Clicked on Vaccines tab");
+        await driver.sleep(6000); // wait for page to load
+
+        // Click on PrescribeCare tab
+        await driver.findElement(By.xpath("//a[@href = '/Patient/PrescribeCare?patientId=AB6002230683436188168042F8DF9D88&timer=true']")).click();
+        console.log("Clicked on PrescribeCare tab");
+        await driver.sleep(6000);
+
+        // Click on Care Campaigns tab
+        await driver.findElement(By.linkText('Care Campaigns')).click();
+        console.log("Clicked on Care Campaigns tab");
+        await driver.sleep(6000); // wait for page to load
+
+        // Click on Communications tab and respective sub-tabs
+        await driver.findElement(By.linkText('Communications')).click();
+        console.log("Clicked on Communications tab");
+        await driver.sleep(6000); // wait for page to load
+
+        await driver.findElement(By.linkText('Fax History')).click();
+        console.log("Clicked on Fax History sub-tab");
+        await driver.sleep(6000); // wait for page to load
+
+        // Click on Log tab
+        await driver.findElement(By.linkText('Log')).click();
+        console.log("Clicked on Log tab");
+        await driver.sleep(6000); // wait for page to load
+
     }
     catch(err) {
         console.log(err);
