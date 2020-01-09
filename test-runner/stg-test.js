c62574de8a1a9efc90390357adafab7a5e55de03
Michael Osgood <michael.f.osgood@gmail.com>
2020-01-09 22:20:38 +0000

    added start and end time

--- test-runner/stg-test.js
+++ test-runner/stg-test.js
@@ -3,9 +3,15 @@ let firefoxStgSmoketest = require('../tests/firefox_stg_smoketest.js');
 
 (async function stgSmoketest() { 
     try {
+        let startTime = new Date();
+        console.log("Smoke Test Started at: " + startTime);
         await googleStgSmoketest();
         await firefoxStgSmoketest();
     } catch (error) {
         throw error;
+    }
+    finally {
+        let endTime = new Date();
+        console.log("Smoke Test Completed at: " + endTime); 
     }
 })();
