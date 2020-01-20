9fdfe36ea72fd50843000e45ff3b3518e0ce5f63
Michael Osgood <michael.f.osgood@gmail.com>
2020-01-20 11:14:16 -0800

    updated pt profile tests

--- tests/firefox_prod_pt_profiles.js
+++ tests/firefox_prod_pt_profiles.js
@@ -1,4 +1,5 @@
 const {Builder, By, Key, actions, until} = require('selenium-webdriver');
+let login = require("../components/login.js");
 let credentials = require('../credentials.js');
 let environment = require('../environment.js');
 let pt_profile = require('../components/patient_profile.js');
@@ -13,87 +14,101 @@ const firefoxProdPtProfiles = async function() {
         await driver.get(environment.prod);
         console.log("Went to Production");
 
-        // Login to User
-        await driver.findElement(By.id('mbr-uid')).sendKeys(credentials.customer_user);
+ // Login to User
+        await driver.findElement(By.id(login.id)).sendKeys(credentials.customer_user);
         console.log("Entered username");
-        await driver.findElement(By.id('mbr-pwd')).sendKeys(credentials.customer_password, Key.RETURN);
+        await driver.findElement(By.id(login.pw)).sendKeys(credentials.customer_password, Key.RETURN);
         console.log("Entered password and clicked 'Enter'");
-        await driver.sleep(6000); // Wait for page to load
-
+        await driver.sleep(6000);
+        console.log("Waited 6 seconds");
+        
         //Verify Dashboard Title
-        await driver.sleep(6000); // Wait for page to load
-        await driver.getTitle().then(function (title) {
+        await driver.getTitle().then(function(title) {
             assert.equal(title, titles.independent_dashboard);
-            console.log("Asserted title is: " + title);
+            console.log("Asserted title is: " + title );
         });
 
         // Go to Patient Profile
         await driver.get(pt_profile.prod_link);
-
+        
         // Click on the Overview tab
         await driver.findElement(By.linkText('Overview')).click();
         console.log("Clicked on Overview tab");
-        await driver.sleep(6000); // wait for page to load
-
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
+        
         // Click on the Programs tab
         await driver.findElement(By.linkText('Programs')).click();
         console.log("Clicked on Programs sub-tab");
-        await driver.sleep(6000); // wait for page to load
-
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
+        
         // Click on the 'Medications' tab
         await driver.findElement(By.linkText('Medications')).click();
         console.log("Clicked on Medication tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         await driver.findElement(By.linkText('Patient-Reported Medication')).click();
         console.log("Clicked on Patient-Reported Medication sub-tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         await driver.findElement(By.linkText('Medication Allergies')).click();
         console.log("Clicked on Medication Allergies sub-tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         await driver.findElement(By.linkText('Medication History')).click();
         console.log("Clicked on Medication History sub-tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         await driver.findElement(By.linkText('Med Time Reminders')).click();
         console.log("Clicked on Med Time Reminders sub-tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(6000);
+        console.log("Waited 6 seconds");
 
         // Click on Medicare tab 
         await driver.findElement(By.linkText('Medicare')).click();
         console.log("Clicked on Medicare tab");
-        await driver.sleep(10000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         // Click on Vaccines tab
         await driver.findElement(By.linkText('Vaccines')).click();
         console.log("Clicked on Vaccines tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         // Click on PrescribeCare tab
         await driver.findElement(By.xpath("//a[@href = '/Patient/PrescribeCare?patientId=AB6002230683436188168042F8DF9D88&timer=true']")).click();
         console.log("Clicked on PrescribeCare tab");
-        await driver.sleep(6000);
+         await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         // Click on Care Campaigns tab
         await driver.findElement(By.linkText('Care Campaigns')).click();
         console.log("Clicked on Care Campaigns tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         // Click on Communications tab and respective sub-tabs
         await driver.findElement(By.linkText('Communications')).click();
         console.log("Clicked on Communications tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         await driver.findElement(By.linkText('Fax History')).click();
         console.log("Clicked on Fax History sub-tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
 
         // Click on Log tab
         await driver.findElement(By.linkText('Log')).click();
         console.log("Clicked on Log tab");
-        await driver.sleep(6000); // wait for page to load
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
     }
     catch(err) {
         console.log(err);
