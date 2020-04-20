dceaa41d2449ce22d2f8bbdb7b98953795e57ac9
Michael Osgood <michael.f.osgood@gmail.com>
2020-04-20 15:33:18 -0700

    added example test for demo purposes

---
+++ test-runner/example_test.js
@@ -0,0 +1,19 @@
+let testOne = require("../tests/test_one.js");
+let testTwo = require("../tests/test_two.js");
+let testThree = require("../tests/test_three.js");
+
+(async function cachingTest() { 
+    try {
+        let startTime = new Date();
+        console.log("Example Test started at: " + startTime);
+        await testOne();
+        await testTwo();
+        await testThree();
+    } catch (error) {
+        throw error;
+    }
+    finally {
+        let endTime = new Date();
+        console.log("Example Test completed at: " + endTime);
+      }
+})();
