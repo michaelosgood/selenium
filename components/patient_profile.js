6743b195218d5321c202a881beaa1fc77cfc02e4
Michael Osgood <michael.f.osgood@gmail.com>
2020-06-10 10:42:42 -0700

    implemented smoke test for Beta

--- components/patient_profile.js
+++ components/patient_profile.js
@@ -4,10 +4,12 @@ let pt_profile = {};
 pt_profile.prod_link = "https://web.prescribewellness.com/Patient/Details?patientId=AB6002230683436188168042F8DF9D88";
 pt_profile.stg_link = "https://webstg.prescribewellness.com/Patient/Details?patientId=AB6002230683436188168042F8DF9D88";
 pt_profile.dev_link = "https://dev-web.prescribewellness.com/Patient/Details?patientId=AB6002230683436188168042F8DF9D88";
+pt_profile.beta_link = "https://beta-web.prescribewellness.com/Patient/Details?patientId=AB6002230683436188168042F8DF9D88";
 
 // Brent Bagwell's pt profile in Sample Chain Store 1 (Account 2043550027)
 pt_profile.dev_chain_link = "https://dev-web.prescribewellness.com/Patient/Details?patientId=204355.01DDDB4C6ECD41D3A3DFCA37E613147A";
 pt_profile.stg_chain_link = "https://webstg.prescribewellness.com/Patient/Details?patientId=204355.01DDDB4C6ECD41D3A3DFCA37E613147A";
 pt_profile.prod_chain_link = "https://web.prescribewellness.com/Patient/Details?patientId=204355.01DDDB4C6ECD41D3A3DFCA37E613147A";
+pt_profile.beta_chain_link = "https://beta-web.prescribewellness.com/Patient/Details?patientId=204355.01DDDB4C6ECD41D3A3DFCA37E613147A";
 
 module.exports = pt_profile;
---
+++ test_runner/google_beta_smoketest.js
@@ -0,0 +1,25 @@
+let googleBetaAdmins = require('../tests/google_beta_admins.js')
+let googleBetaIndependents = require('../tests/google_beta_independents.js');
+let googleBetatProfiles = require('../tests/google_beta_patient_profiles.js');
+let googleBetaChainPartner = require('../tests/google_beta_chain_partner.js');
+let googleBetaChainUserA = require('../tests/google_beta_chain_user_a.js');
+let googleBetaChainUserB = require('../tests/google_beta_chain_user_b.js');
+
+(async function betaSmoketest() { 
+    try {
+        let startTime = new Date();
+        console.log("Smoke Test Started at: " + startTime);
+        await googleBetaAdmins();
+        await googleBetaIndependents();
+        await googleBetaPtProfiles();
+        await googleBetaChainPartner();
+        await googleBetaChainUserA();
+        await googleBetaChainUserB();
+    } catch (error) {
+        throw error;
+    }
+    finally {
+        let endTime = new Date();
+        console.log("Smoke Test Completed at: " + endTime);
+      }
+})();
