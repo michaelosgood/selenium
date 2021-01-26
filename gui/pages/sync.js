afc9e5a3a5ada0463a00eb5419396c7c27f79eca
Michael Osgood <michael.f.osgood@gmail.com>
2021-01-26 15:01:34 -0800

    updated url for med sync page on rebranding

--- gui/pages/sync.js
+++ gui/pages/sync.js
@@ -1,6 +1,6 @@
 let sync = {};
 
-sync.stg = "https://stg-rebranding.prescribewellness.com/MedSync/MedicationReconciliation";
+sync.stg = "https://stg-rebranding.prescribewellness.com/MedSync/MedicationSynchronization";
 sync.title = "Med Sync: Synchronization - Pharmacy Portal - Account: 129634 - NPI: 9876543210 - NCPDP: 1296341 - HthreeNtwo";
 sync.not_enrolled = 'Not Enrolled';
 sync.enrolled = "Enrolled";
