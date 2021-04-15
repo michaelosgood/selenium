9813a64142de621cba5c3d3ad2b6f305dedb5e47
Michael Osgood <michael.f.osgood@gmail.com>
2021-04-15 12:36:45 -0700

    added 2 seconds for int admin dash to load

--- tests/internal_admin/admin.js
+++ tests/internal_admin/admin.js
@@ -22,8 +22,8 @@ const admin_test = async function() {
         console.log("Entered username");
         await driver.findElement(By.id(login.pw)).sendKeys(credentials.internal_password, Key.RETURN);
         console.log("Entered password and clicked 'Enter'");
-        await driver.sleep(3000);
-        console.log("Waited 3 seconds");
+        await driver.sleep(5000);
+        console.log("Waited 5 seconds");
         await driver.getTitle().then(function (title) {
             assert.equal(title, titles.dashboard);
             console.log("Asserted title");
