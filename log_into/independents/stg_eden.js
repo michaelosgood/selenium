a50d2c5b61c55fe8348d3d571c40d5d7a491defd
Michael Osgood <michael.f.osgood@gmail.com>
2021-07-15 15:47:07 -0700

    updated independent logins

---
+++ log_into/independents/stg_eden.js
@@ -0,0 +1,25 @@
+const {Builder, By, Key, until} = require('selenium-webdriver');
+let credentials = require('../../credentials.js');
+let environment = require('../../environment.js');
+let login = require('../../gui/components/login.js');
+
+(async function SignIn() {
+  let driver = await new Builder().forBrowser('chrome').build();
+    try {
+        await driver.get(environment.stg);
+        console.log("Went to Staging");
+        await driver.findElement(By.id(login.id)).sendKeys(credentials.eden_user);
+        console.log("Entered internal username");
+        await driver.findElement(By.id(login.pw)).sendKeys(credentials.eden_password, Key.RETURN);
+        console.log("Entered password and clicked 'Enter'");
+        await driver.sleep(5000); 
+        console.log("Waited 5 seconds");
+
+    } 
+    catch(err) {
+        console.log(err);
+    }
+    finally {
+        console.log("Internal user signed into Dev with Chrome");
+    }
+})();
