3aa3eccf4345a71763166cf9e7d45722a135c312
Michael Osgood <michael.f.osgood@gmail.com>
2021-11-05 12:13:04 -0700

    added aws smoketest

---
+++ test_runner/aws_smoketest.js
@@ -0,0 +1,32 @@
+let admin_test = require('../tests/internal_admin/aws_admin.js');
+let pt_list_test = require('../tests/pharmacies/independents/aws_pt_list.js');
+let pt_profile_test = require('../tests/pharmacies/independents/aws_pt_profile.js');
+let psao_partner_test = require('../tests/partner_admin/psao/aws_dashboard.js');
+let chain_partner_test = require('../tests/partner_admin/chain/aws_dashboard.js');
+let chain_pt_list_test = require('../tests/pharmacies/chains/aws_pt_list.js');
+let startTime ;
+let start ;
+let endTime ;
+let end ;
+
+(async function stgSmoketest() { 
+    try {
+        start = new Date();
+        startTime = new Date().getTime();
+        console.log("AWS Smoketest Started at: 1:00" + start);
+        await admin_test();
+        await pt_list_test();
+        await pt_profile_test();
+        await psao_partner_test();
+        await chain_partner_test();
+        await chain_pt_list_test();
+    } catch (error) {
+        throw error;
+    }
+    finally {
+        end = new Date();
+        endTime = new Date().getTime();
+        console.log("AWS Smoketest Completed at: " + end);
+        console.log("AWS Smoketest Total Time = " + (((endTime - startTime )/1000)/60) + " minutes");
+      }
+})();
